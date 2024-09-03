ed6564d7d4d24a54cab163a2247efe885cc634e3
xuexcy <xuechengyunxue@gmail.com>
2024-09-03 20:17:37 +0800

    add 3.5

---
+++ chapter-3/minigrep/src/main.rs
@@ -0,0 +1,15 @@
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::process;
+
+fn main() {
+    let config = minigrep::Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
